b8c3c44a2d968ad7603302f7976a14d6734102b1
Maria Popleteeva <m.popleteeva@gmail.com>
2020-05-10 00:53:44 +0300

    оптимизирует

--- source/js/oRange.js
+++ source/js/oRange.js
@@ -1,13 +1,15 @@
-"use strict";
-var oRange;
-320 <= window.innerWidth &&
-  window.innerWidth < 768 &&
-  ((oRange = document.querySelector('input[type="range"]')).value = 1),
-  (window.onresize = document.body.clientWidth),
-  window.addEventListener("resize", function (e) {
-    var n = document.body.clientWidth,
-      t = document.querySelector('input[type="range"]');
-    320 <= n && n < 768 && (t.value = 1),
-      768 <= n && n < 1300 && (t.value = 50),
-      1300 < n && (t.value = 52);
-  });
+if (document.querySelector(".slider")) {
+  ("use strict");
+  var oRange;
+  320 <= window.innerWidth &&
+    window.innerWidth < 768 &&
+    ((oRange = document.querySelector('input[type="range"]')).value = 1),
+    (window.onresize = document.body.clientWidth),
+    window.addEventListener("resize", function (e) {
+      var n = document.body.clientWidth,
+        t = document.querySelector('input[type="range"]');
+      320 <= n && n < 768 && (t.value = 1),
+        768 <= n && n < 1300 && (t.value = 50),
+        1300 < n && (t.value = 52);
+    });
+}
